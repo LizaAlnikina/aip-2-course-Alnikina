d3c1d5f690701c88aaac3f9a12d1ac891dab8042
LizaAlnikina <lizaalnikina3@gmail.com>
2025-12-06 12:46:50 +0300

    Update lab6_v1_n2.c

--- lab6_v1_n2.c
+++ lab6_v1_n2.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int main() {
-    int X[6][8] = {
+    int rows = 6, cols = 8;
+    int i, j;
+
+    int** X = (int**)malloc(rows * sizeof(int*));
+    for(i = 0; i < rows; i++) {
+        X[i] = (int*)malloc(cols * sizeof(int));
+    }
+    int initial_data[6][8] = {
         {5, 12, -3, 8, 0, 7, -1, 15},
         {-2, 9, 4, -6, 11, 3, 10, -8},
         {7, -4, 6, 13, -5, 2, 14, 1},
@@ -10,25 +19,75 @@ int main() {
         {2, -12, 13, -10, 8, -4, 7, 18}
     };
     
-    int Xmin = X[0][0];
-    int Xmax = X[0][0];
-    int i, j;
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            X[i][j] = initial_data[i][j];
+        }
+    }
     
-    printf("Матрица X:\n");
-    for(i = 0; i < 6; i++) {
-        for(j = 0; j < 8; j++) {
+    printf("Матрица X (%dx%d):\n", rows, cols);
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
             printf("%4d ", X[i][j]);
-            
-            if(X[i][j] < Xmin) Xmin = X[i][j];
-            if(X[i][j] > Xmax) Xmax = X[i][j];
         }
         printf("\n");
     }
     
-    float Z = (float)Xmin / (Xmax + Xmin);
+    int Xmin_positive = INT_MAX;
+    int Xmax = INT_MIN;
     
-    printf("\nXmin = %d, Xmax = %d\n", Xmin, Xmax);
-    printf("Z = %.4f\n", Z);
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            int current = X[i][j];
+            
+            if(current > 0 && current < Xmin_positive) {
+                Xmin_positive = current;
+            }
+
+            if(current > Xmax) {
+                Xmax = current;
+            }
+        }
+    }
+    
+
+    if(Xmin_positive == INT_MAX) {
+        printf("ОШИБКА: В матрице нет положительных элементов!\n");
+        printf("Невозможно вычислить Z.\n");
+    }
+    else {
+        printf("Минимальный положительный элемент (Xmin): %d\n", Xmin_positive);
+        printf("Максимальный элемент (Xmax): %d\n", Xmax);
+        printf("Сумма: Xmax + Xmin = %d + %d = %d\n", 
+               Xmax, Xmin_positive, Xmax + Xmin_positive);
+        
+        float Z = (float)Xmin_positive / (Xmax + Xmin_positive);
+        
+        printf("\nВычисление Z:\n");
+        printf("Z = Xmin / (Xmax + Xmin)\n");
+        printf("Z = %d / (%d + %d)\n", Xmin_positive, Xmax, Xmin_positive);
+        printf("Z = %d / %d\n", Xmin_positive, Xmax + Xmin_positive);
+        printf("Z = %.6f\n", Z);
+    }
+
+    
+    int positive_count = 0;
+    int negative_count = 0;
+    int zero_count = 0;
+    
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            if(X[i][j] > 0) positive_count++;
+            else if(X[i][j] < 0) negative_count++;
+            else zero_count++;
+        }
+    }
+    
+    for(i = 0; i < rows; i++) {
+        free(X[i]);
+    }
+    free(X);
+    printf("\nПамять освобождена\n");
     
     return 0;
 }
