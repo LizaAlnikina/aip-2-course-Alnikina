77c9f0cc3249869bcc9d3f90c8374c1504bfe53e
LizaAlnikina <lizaalnikina3@gmail.com>
2025-12-15 19:36:35 +0300

    Update lab7_v1_n1.c

--- lab7_v1_n1.c
+++ lab7_v1_n1.c
@@ -1,59 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fill_array(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        arr[i] = rand() % 200 - 100;
+    }
+}
+
+void fill_matrix(int rows, int cols, int matrix[rows][cols]) {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            matrix[i][j] = rand() % 200 - 100;
+        }
+    }
+}
+
+int find_array_max(int arr[], int size, int *index) {
+    int max = arr[0];
+    *index = 0;
+    for(int i = 1; i < size; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+            *index = i;
+        }
+    }
+    return max;
+}
+
+int find_matrix_max(int rows, int cols, int matrix[rows][cols], 
+                    int *row, int *col) {
+    int max = matrix[0][0];
+    *row = 0;
+    *col = 0;
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            if(matrix[i][j] > max) {
+                max = matrix[i][j];
+                *row = i;
+                *col = j;
+            }
+        }
+    }
+    return max;
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main() {
     int A[57];
     int B[7][5];
     
-    int i, j;
-
-    printf("A (57)\n");
-    for(i = 0; i < 57; i++) {
-        A[i] = rand() % 200 - 100;
-        printf("%4d ", A[i]);
-        if((i+1) % 10 == 0) printf("\n");
-    }
-    printf("\n\n");
+    fill_array(A, 57);
+    fill_matrix(7, 5, B);
     
-    printf("B (7x5)\n");
-    for(i = 0; i < 7; i++) {
-        for(j = 0; j < 5; j++) {
-            B[i][j] = rand() % 200 - 100;
-            printf("%4d ", B[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-
-    int max_A = A[0];
-    int max_A_index = 0;
-    for(i = 1; i < 57; i++) {
-        if(A[i] > max_A) {
-            max_A = A[i];
-            max_A_index = i;
-        }
-    }
-    printf("Максимум в A: A[%d] = %d\n", max_A_index, max_A);
-
-    int max_B = B[0][0];
-    int max_B_row = 0, max_B_col = 0;
-    for(i = 0; i < 7; i++) {
-        for(j = 0; j < 5; j++) {
-            if(B[i][j] > max_B) {
-                max_B = B[i][j];
-                max_B_row = i;
-                max_B_col = j;
-            }
-        }
-    }
-    printf("Максимум в B: B[%d][%d] = %d\n\n", max_B_row, max_B_col, max_B);
-
-    A[max_A_index] = max_B;
-    B[max_B_row][max_B_col] = max_A;
+    int a_idx, b_row, b_col;
+    int max_a = find_array_max(A, 57, &a_idx);
+    int max_b = find_matrix_max(7, 5, B, &b_row, &b_col);
     
-    printf("После обмена максимумами:\n");
-    printf("A[%d] теперь = %d\n", max_A_index, A[max_A_index]);
-    printf("B[%d][%d] теперь = %d\n", max_B_row, max_B_col, B[max_B_row][max_B_col]);
+    swap(&A[a_idx], &B[b_row][b_col]);
+    
+    printf("максимум в A был: %d\n", max_a);
+    printf("максимум в B был: %d\n", max_b);
+    printf("после обмена: A[%d]=%d, B[%d][%d]=%d\n", 
+           a_idx, A[a_idx], b_row, b_col, B[b_row][b_col]);
     
     return 0;
 }
